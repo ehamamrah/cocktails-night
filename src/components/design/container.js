edaf5571dbe05150c8b7aa543afcc0c6d387b45e
ehamamrah <elayan.hr@gmail.com>
2020-10-17 05:28:21 +0300

    Fixed List

--- src/components/design/container.js
+++ src/components/design/container.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import axios from 'axios';
-import Drink from './container';
 
 
 export default class Collector extends React.Component {
   state={
     drinks: [],
+    drink: [],
   };
 
   componentDidMount() {
-    const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita';
-    // const url = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail';
+    const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
     axios.get(url)
       .then(res => {
         const drinks = res.data['drinks'];
@@ -18,14 +17,41 @@ export default class Collector extends React.Component {
       })
   }
 
+  getDrink(drinkName){
+    const drink_details_link = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=' + drinkName;
+    axios.get(drink_details_link)
+      .then(res => {
+        const details = res.data;
+        const drink = details['drinks'][0];
+        this.setState({ drink });
+      })
+  }
+
   render() {
     return (
       <>
-        <div className="container">
-          <div className="row">
-            { this.state.drinks.map(drink =>
-              < Drink drink_name={drink.strDrink}/>
-            )}
+        <div className="album py-5 bg-light-gray">
+          <div className="container">
+            <div className="row">
+              { this.state.drinks.map(drink =>
+                <div className="col-4">
+                  <div className="card mb-4 shadow-sm">
+                    <img className="bd-placeholder-img card-img-top shadow-sm rounded-top" width="100%" height="225" src={drink.strDrinkThumb} role="img" aria-label="Placeholder: Thumbnail" />
+                    <div className="card-body">
+                    <h5 className="card-title">{drink.strDrink}</h5>
+                      <p className="card-text text-justify text-truncate">{drink.strInstructions}</p>
+                      <div className="d-flex justify-content-between align-items-center">
+                        <div className="btn-group">
+                          <button type="button" className="btn btn-sm btn-outline-warning">Read More ..</button>
+                        </div>
+                        {/* { this.getDrink(drink.strDrink) } */}
+                        <small className="text-muted">{drink.strAlcoholic}, {drink.strGlass}</small>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              )}
+            </div>
           </div>
         </div>
       </>
